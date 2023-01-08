7390e5d932189fb23bdb6b47d2b17f7c679ec62c
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-01-08 23:43:33 +0200

    3rd app for the graphics gw

---
+++ Grafica/Aplicatie 3/main.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+unsigned int shader;
+
+static std::string readShader(const std::string& filePath)
+{
+    std::ifstream in(filePath);
+    std::stringstream ss;
+
+    if (!in)
+    {
+        std::cerr << "Could not open " << filePath << '\n';
+    }
+
+    ss << in.rdbuf();
+    in.close();
+
+    return ss.str();
+}
+
+static unsigned int compileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader\n";
+        std::cout << message << '\n';
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shader);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+    if (h == 0)
+        h = 1;
+    float ratio = w * 1.0 / h;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    gluPerspective(45.f, ratio, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100, 200);
+    glutInitWindowSize(400, 400);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutCreateWindow("Aplicatia 3");
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cerr << "Error " << glewGetErrorString(err) << '\n';
+    }
+
+    float positions[9] =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << '\n';
+    std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), &positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+    std::string vertexShader = readShader("../shader.vert");
+    std::string fragmentShader = readShader("../shader.frag");
+
+    shader = createShader(vertexShader, fragmentShader);
+
+    glutDisplayFunc(render);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(render);
+
+    glutMainLoop();
+
+    return 0;
+}
