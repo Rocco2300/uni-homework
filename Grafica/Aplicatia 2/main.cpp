0d9b40cf88b2f69eca591c27066fdf5040eb06a7
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-01-08 23:43:16 +0200

    second app for graphics end of sem hw

---
+++ Grafica/Aplicatia 2/main.cpp
@@ -0,0 +1,107 @@
+#include <GL/freeglut.h>
+
+float angleX = 0.0f;
+float angleY = 0.0f;
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    gluLookAt(	0.0f, 0.0f, 10.0f,
+                  0.0f, 0.0f,  0.0f,
+                  0.0f, 1.0f,  0.0f);
+
+    glRotatef(angleX, 1.0f, 0.0f, 0.0f);
+    glRotatef(angleY, 0.0f, 1.0f, 0.0f);
+
+    // fata
+    glColor3f(0.25, 0.52, 0.96);
+    glBegin(GL_QUADS);
+    glVertex3f(-1.0, 1.0, 1.0);
+    glVertex3f(1.0, 1.0, 1.0);
+    glVertex3f(1.0, -1.0, 1.0);
+    glVertex3f(-1.0, -1.0, 1.0);
+    glEnd();
+
+    // spate
+    glColor3f(0.98, 0.72, 0.01);
+    glBegin(GL_QUADS);
+    glVertex3f(-1.0, 1.0, -1.0);
+    glVertex3f(1.0, 1.0, -1.0);
+    glVertex3f(1.0, -1.0, -1.0);
+    glVertex3f(-1.0, -1.0, -1.0);
+    glEnd();
+
+    // dreapta
+    glColor3f(0.92, 0.25, 0.20);
+    glBegin(GL_QUADS);
+    glVertex3f(1.0, 1.0, 1.0);
+    glVertex3f(1.0, 1.0, -1.0);
+    glVertex3f(1.0, -1.0, -1.0);
+    glVertex3f(1.0, -1.0, 1.0);
+    glEnd();
+
+    // sus
+    glColor3f(0.19, 0.65, 0.32);
+    glBegin(GL_QUADS);
+    glVertex3f(1.0, 1.0, 1.0);
+    glVertex3f(1.0, 1.0, -1.0);
+    glVertex3f(-1.0, 1.0, -1.0);
+    glVertex3f(-1.0, 1.0, 1.0);
+    glEnd();
+
+    // jos
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_QUADS);
+    glVertex3f(-1.0, -1.0, 1.0);
+    glVertex3f(1.0, -1.0, 1.0);
+    glVertex3f(1.0, -1.0, -1.0);
+    glVertex3f(-1.0, -1.0, -1.0);
+    glEnd();
+
+    glColor3f(0.92, 0.50, 0.13);
+    glBegin(GL_QUADS);
+    glVertex3f(-1.0, 1.0, 1.0);
+    glVertex3f(-1.0, -1.0, 1.0);
+    glVertex3f(-1.0, -1.0, -1.0);
+    glVertex3f(-1.0, 1.0, -1.0);
+    glEnd();
+
+    angleX += 0.02f;
+    angleY += 0.03f;
+
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+    if (h == 0)
+        h = 1;
+    float ratio =  w * 1.0 / h;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100, 200);
+    glutInitWindowSize(400, 400);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutCreateWindow("Cube?");
+
+    glEnable(GL_DEPTH_TEST);
+
+    glutDisplayFunc(render);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(render);
+
+    glutMainLoop();
+
+    return 0;
+}
